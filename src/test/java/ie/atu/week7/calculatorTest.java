c076b6a5090987d767dc6841fc92ec65efb581cb
eoin <g00431577@atu.ie>
2025-04-27 01:53:12 +0100

    calculator test class created

---
+++ src/test/java/ie/atu/week7/calculatorTest.java
@@ -0,0 +1,16 @@
+package ie.atu.week7;
+
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class calculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void test() {}
+}
