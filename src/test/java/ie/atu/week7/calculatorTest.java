782af23524bbfc9254e0fb16c86665c3a2a5db76
eoin <g00431577@atu.ie>
2025-04-27 01:57:00 +0100

    feaure add test

--- src/test/java/ie/atu/week7/calculatorTest.java
+++ src/test/java/ie/atu/week7/calculatorTest.java
@@ -6,11 +6,15 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class calculatorTest {
+    calculator cal;
 
     @BeforeEach
     void setUp() {
+        cal = new calculator();
     }
 
     @Test
-    void test() {}
+    void testadd() {
+        assertEquals(5, cal.add(3,2));
+    }
 }
